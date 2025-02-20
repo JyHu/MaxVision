84827cd1c31ecf1cb5b5fd9d510447587c32bde9
hujinyou <hujinyou@itiger.com>
2025-02-20 11:35:17 +0800

    update

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const indexContent = `
+超视觉是一款专为测试颜色辨识度而设计的应用。它能够根据你的设定生成不同布局和颜色属性的色块矩阵，你的任务是在众多色块中找出那个独特的不同色块。
+
+为了更好地完成测试，你可以在"帮助"中查看现有的辅助选项，或添加更多辅助功能，以提升体验。
+
+Max Vision is an app specifically designed to test color recognition ability. It can generate color block matrices with different layouts and color properties based on your settings. Your task is to find the unique color block among many others.
+
+To complete the test more effectively, you can check the existing assistance options in the "Help" section or add more features to enhance your experience.
+
+<br>
+<div class="screenshot-grid">
+    <div class="screenshot-item">
+        <h3>Test View</h3>
+        <img src="res/img01.png" alt="Test View">
+    </div>
+    <div class="screenshot-item">
+        <h3>Help View</h3>
+        <img src="res/img03.png" alt="Help View">
+    </div>
+    <div class="screenshot-item">
+        <h3>Setting View</h3>
+        <img src="res/img04.png" alt="Setting View">
+    </div>
+    <div class="screenshot-item">
+        <h3>Languages</h3>
+        <img src="res/img05.png" alt="Languages">
+    </div>
+</div>
+`; 
